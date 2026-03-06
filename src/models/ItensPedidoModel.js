ac389715777b1cb76324e4ed78b9dd736414ee86
Beatriz Miotto <miotto.beatriz2008@gmail.com>
2026-03-06 09:42:14 -0300

    arruma itensPedidoModel

--- src/models/ItensPedidoModel.js
+++ src/models/ItensPedidoModel.js
@@ -1,16 +1,22 @@
-import prisma from "../utils/prismaClient.js";
+import prisma from '../utils/prismaClient.js';
 
 export default class ItemPedido {
-  constructor({
-    id = null,
-    pedidoId = null,
-    produtoId = null,
-    quantidade,
-    precoUnitario,
-  } = {}) {
-    ((this.id = id), (this.pedidoId = pedidoId));
-    this.produtoId = produtoId;
-    this.quantidade = quantidade;
-    this.precoUnitario = precoUnitario;
-  }
+    constructor({ id = null, pedidoId = null, produtoId = null, quantidade, precoUnitario } = {}) {
+        this.id = id;
+        this.pedidoId = pedidoId;
+        this.produtoId = produtoId;
+        this.quantidade = quantidade;
+        this.precoUnitario = precoUnitario;
+    }
+
+    async criar() {
+        return prisma.itemPedido.create({
+            data: {
+                pedidoId: this.pedidoId,
+                produtoId: this.produtoId,
+                quantidade: this.quantidade,
+                precoUnitario: this.precoUnitario,
+            },
+        });
+    }
 }
