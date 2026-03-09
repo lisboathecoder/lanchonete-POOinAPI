d08e9759444bf7d8bddc30f3a4cc0309d233de19
lisboathecoder <gustavo.lisboa2009@gmail.com>
2026-03-09 11:31:26 -0300

    fix produtosModel

--- src/models/ProdutosModel.js
+++ src/models/ProdutosModel.js
@@ -17,15 +17,16 @@ export default class ProdutosModel {
         if (!this.categoria) {
             throw new Error('O campo "categoria" é obrigatório!');
         }
-        if (this.preco === undefined || preco <= 0) {
+        if (this.preco === undefined || this.preco <= 0) {
             throw new Error('O "preco" deve ser definido e ser maior que 0');
         }
-        if (this.descricao.length >= 255) {
+        if (this.descricao && this.descricao.length >= 255) {
             throw new Error('O campo "descricao" pode ter no máximo apenas 255 caracteres');
         }
         if (this.disponivel === false) {
-            throw new Error('O produto não pode ser adicionado com indisponível');
+            throw new Error("O produto não pode ser adicionado com indisponível");
         }
+
         return prisma.produto.create({
             data: {
                 nome: this.nome,
@@ -35,83 +36,72 @@ export default class ProdutosModel {
                 disponivel: this.disponivel ?? true
             }
         });
-        }
-
-  async criar() {
-    return prisma.produto.create({
-      data: {
-        nome: this.nome,
-        descricao: this.descricao,
-        categoria: this.categoria,
-        preco: this.preco,
-        disponivel: this.disponivel,
-      },
-    });
-  }
-
-  async atualizar() {
-    return prisma.produto.update({
-      where: { id: this.id },
-      data: {
-        nome: this.nome,
-        descricao: this.descricao,
-        categoria: this.categoria,
-        preco: this.preco,
-        disponivel: this.disponivel,
-      },
-    });
-  }
-
-  async deletar() {
-    const itemPedidoAberto = await prisma.itemPedido.findFisrt({
-      where: {
-        produtoID: this.id,
-        pedido: { status: "ABERTO" },
-      },
-    });
-
-    if (itemPedidoAberto) {
-      return {
-        status: 400,
-        error: "Não é possível deletar um produto vinculado a pedido aberto",
-      };
     }
 
-      static async buscarTodos(filtro = {}) {
+    async atualizar() {
+        return prisma.produto.update({
+            where: { id: this.id },
+            data: {
+                nome: this.nome,
+                descricao: this.descricao,
+                categoria: this.categoria,
+                preco: this.preco,
+                disponivel: this.disponivel
+            }
+        });
+    }
+
+    async deletar() {
+        const itemPedidoAberto = await prisma.itemPedido.findFirst({
+            where: {
+                produtoID: this.id,
+                pedido: { status: "ABERTO" }
+            }
+        });
+
+        if (itemPedidoAberto) {
+            return {
+                status: 400,
+                error: "Não é possível deletar um produto vinculado a pedido aberto"
+            };
+        }
+
+        return prisma.produto.delete({
+            where: { id: this.id }
+        });
+    }
+
+    static async buscarTodos(filtros = {}) {
         const where = {};
 
-        if(filtro.nome) {
-            where.nome= {
-                contains: filtro.nome,
-                mode: "insensitive"
-            };
-        }
-        if (filtro.categoria) {
-            where.categoria ={
-                contains: filtro.categoria,
+        if (filtros.nome) {
+            where.nome = {
+                contains: filtros.nome,
                 mode: "insensitive"
             };
         }
 
-         if (filtro.disponivel !== undefined){
-            where.disponivel = filtro.disponivel === true;
+        if (filtros.categoria) {
+            where.categoria = {
+                contains: filtros.categoria,
+                mode: "insensitive"
+            };
+        }
 
-         }
+        if (filtros.disponivel !== undefined) {
+            where.disponivel = filtros.disponivel === true;
+        }
 
-    return { status: 200 };
-  }
+        if (filtros.precoMax) {
+            where.preco = {
+                lte: parseFloat(filtros.precoMax)
+            };
+        }
 
-  static async buscarTodos(filtros = {}) {
-    const where = {};
+        return prisma.produto.findMany({ where });
+    }
 
-         if (filtro.precoMax) {
-            where.preco.lte = parseFloat(filtro.precoMax);
-         }
-         return prisma.produto.findMany({ where});
-
-      }
-
-  static async buscarPorId(id) {
-    return prisma.produto.findUnique({ where: { id } });
-  }
+    static async buscarPorId(id) {
+        return prisma.produto.findUnique({ where: { id } });
+    }
 }
