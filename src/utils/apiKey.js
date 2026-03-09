50e731dd846d148e6548ed4390bbce4f61174f51
Rafael-1108 <rafael.s.mendes.1108@gmail.com>
2026-03-09 11:50:20 -0300

    arruma apiKey

--- src/utils/apiKey.js
+++ src/utils/apiKey.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 
 const auth = (req, res, next) => {
   const apiKey = req.headers['x-api-key'];
-  if (apiKey !== process.env.API_KEY) {
+  if (!apiKey || apiKey !== process.env.API_KEY) {
     return res.status(401).json({ error: "Chave de API inválida." });
   }
   next();
