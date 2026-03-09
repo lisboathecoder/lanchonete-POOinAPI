b008768153304543c43445968a4130f2abb7cd29
Ana <ana.c.luiz@aluno.senai.br>
2026-03-09 08:06:05 -0300

    ajustes nas regras de negócio

--- src/controllers/produtoController.js
+++ src/controllers/produtoController.js
@@ -1,4 +1,4 @@
-import ProdutosModel from '../models/ProdutosModel.js';
+import ProdutosModel from "../models/ProdutosModel.js";
 
 export const criar = async (req, res) => {
     try {
@@ -9,102 +9,112 @@ export const criar = async (req, res) => {
         }
         const {nome, descricao, categoria, preco, disponivel = true } = req.body;
 
-        if (!nome) return res.status(400).json({ error: 'O campo "nome" é obrigatório!' });
+        if (!nome || nome.trim().length < 3) return res.status(400).json({ error: 'O campo "nome" é obrigatório e precisa ter pelo menos três caracteres.' });
         if (!categoria) return res.status(400).json({ error: 'O campo "categoria" é obrigatório!' });
-        if (!preco) return res.status(400).json({ error: 'O campo "preco" é obrigatório!' });
-        if (disponivel === false) return res.status(400).json({error: 'O produto não pode ser adicionado com indisponível'});
+        if (preco === undefined || preco <= 0) return res.status(400).json({ error: 'O "preco" deve ser definido e ser maior que 0' });
+         if (descricao.length >= 255) return res.status(400).json({ error: 'O campo "descricao" pode ter no máximo apenas 255 caracteres' });
+        if (disponivel === false) return res.status(400).json({ error: 'O produto não pode ser adicionado com indisponível' });
 
-        const parsedPrice = parseInt(preco);
-        if (isNaN(parsedPrice)) {
+
+        const precoehNumero = parseInt(preco);
+        if (isNaN(precoehNumero)) {
             return res.status(400).json({ error: 'O preço precisa ser uma número válido'})
         };
 
-        if (preco <= 0) {
-            return res.status(400).json({
-                error: 'O preço precisa ser positivo'
-            })
-        };
+    const produto = new ProdutosModel(nome, descricao, categoria, preco, ativo);
+    const data = await produto.criar();
 
-        const produto = new ProdutosModel(nome, descricao, categoria, preco, ativo);
-        const data = await produto.criar();
-
-        res.status(201).json({ message: 'Registro criado com sucesso!', data });
-
-
-    } catch (error) {
-        console.error('Erro ao criar:', error);
-        res.status(500).json({ error: 'Erro interno ao salvar o registro.' });
-    }
+    res.status(201).json({ message: "Registro criado com sucesso!", data });
+  } catch (error) {
+    console.error("Erro ao criar:", error);
+    res.status(500).json({ error: "Erro interno ao salvar o registro." });
+  }
 };
 
 export const buscarTodos = async (req, res) => {
-    try {
-        const produto = new ProdutosModel();
-        const listarProdutos = await produto.buscarTodos(req.query);
+  try {
+    const produto = new ProdutosModel();
+    const listarProdutos = await produto.buscarTodos(req.query);
 
-        if (!listarProdutos || listarProdutos.length === 0) {
-            return res.status(200).json({ message: 'Nenhum registro encontrado.' });
-        }
-        res.json(listarProdutos);
-    } catch (error) {
-        console.error('Erro ao buscar:', error);
-        res.status(500).json({ error: 'Erro ao buscar registros.' });
+    if (!listarProdutos || listarProdutos.length === 0) {
+      return res.status(200).json({ message: "Nenhum registro encontrado." });
     }
+    res.json(listarProdutos);
+  } catch (error) {
+    console.error("Erro ao buscar:", error);
+    res.status(500).json({ error: "Erro ao buscar registros." });
+  }
 };
 
+
+
 export const buscarPorId = async (req, res) => {
-    try {
-        const { id } = req.params;
-        if (isNaN(id)) {
-            return res.status(400).json({ error: 'O ID enviado não é um número válido.' });
-        }
-        const produto = new ProdutosModel(parseInt(id));
-        const data = await produto.buscarPorId(id);
-        if (!data) {
-            return res.status(404).json({ error: 'Registro não encontrado.' });
-        }
-        res.json({ data });
-    } catch (error) {
-        console.error('Erro ao buscar:', error);
-        res.status(500).json({ error: 'Erro ao buscar registro.' });
+  try {
+    const { id } = req.params;
+    if (isNaN(id)) {
+      return res
+        .status(400)
+        .json({ error: "O ID enviado não é um número válido." });
     }
+    const produto = new ProdutosModel(parseInt(id));
+    const data = await produto.buscarPorId(id);
+    if (!data) {
+      return res.status(404).json({ error: "Registro não encontrado." });
+    }
+    res.json({ data });
+  } catch (error) {
+    console.error("Erro ao buscar:", error);
+    res.status(500).json({ error: "Erro ao buscar registro." });
+  }
 };
 
 export const atualizar = async (req, res) => {
-    try {
-        const { id } = req.params;
-        if (isNaN(id)) return res.status(400).json({ error: 'ID inválido.' });
-        if (!req.body) {
-            return res.status(400).json({ error: 'Corpo da requisição vazio. Envie os dados!' });
-        }
-        const produto = new ProdutosModel(parseInt(id));
-        const produtoExiste = await produto.buscarPorId();
-        if (!produtoExiste) {
-            return res.status(404).json({ error: 'Registro não encontrado para atualizar.' });
-        }
-
-        const data = await produto.atualizar();
-        res.json({ message: `O registro "${data.nome}" foi atualizado com sucesso!`, data });
-    } catch (error) {
-        console.error('Erro ao atualizar:', error);
-        res.status(500).json({ error: 'Erro ao atualizar registro.' });
+  try {
+    const { id } = req.params;
+    if (isNaN(id)) return res.status(400).json({ error: "ID inválido." });
+    if (!req.body) {
+      return res
+        .status(400)
+        .json({ error: "Corpo da requisição vazio. Envie os dados!" });
     }
+    const produto = new ProdutosModel(parseInt(id));
+    const produtoExiste = await produto.buscarPorId();
+    if (!produtoExiste) {
+      return res
+        .status(404)
+        .json({ error: "Registro não encontrado para atualizar." });
+    }
+
+    const data = await produto.atualizar();
+    res.json({
+      message: `O registro "${data.nome}" foi atualizado com sucesso!`,
+      data,
+    });
+  } catch (error) {
+    console.error("Erro ao atualizar:", error);
+    res.status(500).json({ error: "Erro ao atualizar registro." });
+  }
 };
 
 export const deletar = async (req, res) => {
-    try {
-        const { id } = req.params;
-        if (isNaN(id)) return res.status(400).json({ error: 'ID inválido.' });
+  try {
+    const { id } = req.params;
+    if (isNaN(id)) return res.status(400).json({ error: "ID inválido." });
 
-        const produto = new ProdutosModel(parseInt(id));
-        const produtoExiste = await produto.buscarPorId();
-        if (!produtoExiste) {
-            return res.status(404).json({ error: 'Registro não encontrado para deletar.' });
-        }
-        await produto.deletar();
-        res.json({ message: `O registro "${exists.nome}" foi deletado com sucesso!`, deletado: exists });
-    } catch (error) {
-        console.error('Erro ao deletar:', error);
-        res.status(500).json({ error: 'Erro ao deletar registro.' });
+    const produto = new ProdutosModel(parseInt(id));
+    const produtoExiste = await produto.buscarPorId();
+    if (!produtoExiste) {
+      return res
+        .status(404)
+        .json({ error: "Registro não encontrado para deletar." });
     }
+    await produto.deletar();
+    res.json({
+      message: `O registro "${exists.nome}" foi deletado com sucesso!`,
+      deletado: exists,
+    });
+  } catch (error) {
+    console.error("Erro ao deletar:", error);
+    res.status(500).json({ error: "Erro ao deletar registro." });
+  }
 };
